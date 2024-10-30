ef91d8be985087b3c598195347a970ba6f6df860
jiim123 <jimandersson21@gmail.com>
2024-10-30 16:32:35 +0100

    Updates again!

--- frontend/src/@components/minikit-provider.tsx
+++ frontend/src/@components/minikit-provider.tsx
@@ -1,13 +1,22 @@
 "use client"; // Required for Next.js
 
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { MiniKit } from "@worldcoin/minikit-js";
 
 export default function MiniKitProvider({ children }: { children: ReactNode }) {
+  const [isMiniKitInstalled, setIsMiniKitInstalled] = useState(false);
+
   useEffect(() => {
-    MiniKit.install();
-    console.log(MiniKit.isInstalled());
+    if (typeof window !== "undefined") {
+      MiniKit.install();
+      setIsMiniKitInstalled(MiniKit.isInstalled());
+    }
   }, []);
 
-  return <>{children}</>;
+  return (
+    <>
+      {isMiniKitInstalled ? "MiniKit is installed!" : "MiniKit is not installed."}
+      {children}
+    </>
+  );
 }
